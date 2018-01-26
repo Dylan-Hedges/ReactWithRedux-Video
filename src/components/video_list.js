6196aa9346e000e0714c7b95465f03cfd3c194e3
dylan <d.hedges1@hotmail.co.uk>
2018-01-26 13:40:41 +0000

    Inital Commit

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import VideoListItem from './components/video_list_item';
+import VideoListItem from './video_list_item';
 
 //(props) refers to "videos={this.state.videos}" passed in from index.js
 const VideoList = (props) => {
+    //Using ".map" we can loop through the videos and execute a function for each iteration (built in iterator and alternative to for loops)
+    const videoItems = props.videos.map((video) => {
+        //key={video.etag} - takes the "etag" for each video (as returned by the YouTube API) and assigns it to each video as its key (removes error messages)
+        return <VideoListItem key={video.etag} video={video} />
+    })
+    
     return (
-        //We use className="" instead of class="" because of naming conflicts when we define "class" components
+        //We use className="" instead of class="" because of naming conflicts when we define "class" components, React can see it is an array of items and try to render all the <li> inside of the <ul>
         <ul className="col-md-4 list-group">
-            
+            {videoItems}
         </ul>
     );
 }
